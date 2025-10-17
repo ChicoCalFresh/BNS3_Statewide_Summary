102d4b915ffa9cc89bacb0a7c26d05d95312fa82
Saul Mooradian <slmooradian@csuchico.edu>
2025-10-17 14:36:01 -0700

    pushing everything just in case

---
+++ eop_response_rate.R
@@ -0,0 +1,4 @@
+df <- data.frame(
+  Campus = c("Butte CC", "Cal State LA", "San Francisco State", "Mt. SAC CC", "Clovis CC", "Cal State San Bernardino", "Sacramento State", "Palo Verde CC", "CSU Dominguez Hills", "CSU Bakersfield", "Allan Hancock Community College", "UC Berkeley"),
+  Count = c(387, 2663, 2743, 1129, 481, 1135, 1229, 159, 1764, 702, 858, 600)
+)
